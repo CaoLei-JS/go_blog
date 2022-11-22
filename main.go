ba7829971fea12010299b3957cc5c00022c256b4
开发部/曹磊 <cao870@biox.org.cn>
2022-11-22 10:12:53 +0800

    新增关于页面

--- main.go
+++ main.go
@@ -6,7 +6,18 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+	cur_path := r.URL.Path
+	switch cur_path {
+	case "/":
+		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+	case "/about":
+		fmt.Fprint(w, "此11博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+	default:
+		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
+			"<p>如有疑惑，请联系我们。</p>")
+
+	}
 }
 func main() {
 	http.HandleFunc("/", handlerFunc)
