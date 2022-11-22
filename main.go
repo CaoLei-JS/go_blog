ed21d8b6dcec9656aaa776d9542b41450685a66f
开发部/曹磊 <cao870@biox.org.cn>
2022-11-22 10:19:55 +0800

    自动重载

--- main.go
+++ main.go
@@ -6,20 +6,18 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	cur_path := r.URL.Path
-	switch cur_path {
-	case "/":
-		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
-	case "/about":
-		fmt.Fprint(w, "此11博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
 			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
-	default:
+	} else {
 		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
 			"<p>如有疑惑，请联系我们。</p>")
-
 	}
 }
+
 func main() {
 	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe("localhost:3000", nil)
 }
