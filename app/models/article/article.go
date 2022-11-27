2bb34779c4c71eda3c41c69ac33e0d4fbc919110
开发部/曹磊 <cao870@biox.org.cn>
2022-11-27 14:09:15 +0800

    重构文章列表页面

--- app/models/article/article.go
+++ app/models/article/article.go
@@ -1,8 +1,17 @@
 package article
 
+import (
+	"goblog/pkg/route"
+	"strconv"
+)
+
 // Article 文章模型
 type Article struct {
 	ID    uint64
 	Title string
 	Body  string
+}
+
+func (article Article) Link() string {
+	return route.Name2URL("articles.show", "id", strconv.FormatUint(article.ID, 10))
 }
