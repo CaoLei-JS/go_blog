00fb341912a9d24a5b6ef0f19565cd96dfd14839
开发部/曹磊 <cao870@biox.org.cn>
2022-11-30 14:24:41 +0800

    文章作者

--- app/models/article/article.go
+++ app/models/article/article.go
@@ -2,6 +2,7 @@ package article
 
 import (
 	"goblog/app/models"
+	"goblog/app/models/user"
 	"goblog/pkg/route"
 	"strconv"
 )
@@ -9,10 +10,17 @@ import (
 // Article 文章模型
 type Article struct {
 	models.BaseModel
-	Title string `gorm:"type:varchar(255);not null;" valid:"title"`
-	Body  string `gorm:"type:longtext;not null;" valid:"body"`
+	Title  string `gorm:"type:varchar(255);not null;" valid:"title"`
+	Body   string `gorm:"type:longtext;not null;" valid:"body"`
+	UserID uint64 `gorm:"not null;index"`
+	User   user.User
 }
 
 func (article Article) Link() string {
 	return route.Name2URL("articles.show", "id", strconv.FormatUint(article.ID, 10))
+}
+
+// CreatedAtDate 创建日期
+func (article Article) CreatedAtDate() string {
+	return article.CreatedAt.Format("2006-01-02")
 }
